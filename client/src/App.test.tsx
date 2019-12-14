b7d6739df8ab4c5351e1f0d0fd59edd755cabbf2
Kirill <kirik.yes@gmail.com>
2019-12-14 19:10:23 +0300

    Added rat select input

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 import App from "./App";
-import { render } from "@testing-library/react";
+import {render, waitForElement} from "@testing-library/react";
+import fetchMock from "fetch-mock";
+import {API_URL} from "./constants";
 
-it("renders without crashing", () => {
-  render(<App />);
+beforeAll(() => {
+  // @ts-ignore
+  global.fetch = fetch;
+});
+
+afterAll(() => {
+  fetchMock.restore();
+});
+
+it("rads select renders successfully", async () => {
+  fetchMock.restore().get(`begin:${API_URL}`, ['rat 1', 'rat 2']);
+
+  const { getByTestId, getByText } = render(<App />);
+  const ratSelectNode = await waitForElement(() =>
+    getByTestId('rat-select')
+  );
+  expect(ratSelectNode).toMatchSnapshot();
+  expect(getByText('No Rat')).toBeDefined();
+  expect(getByText('rat 1')).toBeDefined();
 });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,58 @@
-import "destyle.css";
-import React from "react";
+import 'destyle.css';
+import './App.css';
+import React, {useState, useEffect} from 'react';
+import {API_URL} from './constants';
 
 const App: React.FC = () => {
-  return <div>list of RATS That should stay away from my DAMN BINS</div>;
+  const [ratsNames, setRatsNames] = useState([]);
+  const [selectedRat, setSelectedRat] = useState<Rat | null>();
+
+  useEffect(() => {
+    fetch(`${API_URL}/rat-names`)
+      .then(res => res.json())
+      .then(response => {
+        setRatsNames(response);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  async function onRatChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const selectedRatName = event.target.value;
+    if (!selectedRatName) {
+      setSelectedRat(null);
+      return;
+    }
+
+    const response = await fetch(`${API_URL}/rat/${selectedRatName}`);
+    setSelectedRat(await response.json());
+  }
+
+  return (
+    <div className="container">
+      <div className="select" data-testid="rat-select">
+        <select onChange={onRatChange}>
+          <option value="">No Rat</option>
+          {
+            ratsNames.map(name => <option key={name} value={name}>{name}</option>)
+          }
+        </select>
+      </div>
+
+      {
+        selectedRat &&
+        <div className="rat-details">
+            <span>Width: {selectedRat.width}</span>
+            <span>Height: {selectedRat.height}</span>
+            <span>Nickname: {selectedRat.nickname || 'Uncool Rat with no Nickname'}</span>
+        </div>
+      }
+    </div>
+  );
 };
 
+export type Rat = {
+  width: string;
+  height: string;
+  nickname?: string;
+}
 export default App;
